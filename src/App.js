4868d0bd75d1c7dc9f7692c44b92cf16688375c1
Deepak solanki <deepak.solanki2335@gmail.com>
2022-11-03 21:30:20 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-//import logo from './logo.svg';
 import './App.css';
 import React from 'react'
+import Navbar from './page/Navbar.js'
 import Home from './Page/Home'
 import Service from './Page/Service'
 import Contact from './Page/Contact'
 import About from './Page/About'
-import { Route, Redirect } from 'react-router-dom';
-import { MemoryRouter as Router } from 'react-router-dom';
+import { BrowserRouter , Redirect, Route , Routes, Switch } from 'react-router-dom';
+// import { MemoryRouter as Router } from 'react-router-dom';
 //import React, { Component } from 'react';
-
+//import '../node_modules/bootstrap/dist/bootstrap.min.css';
 
 function App() {
   return (
     <>
-      <Router>
+      <Navbar title='Texttile'/>
+      <BrowserRouter>
+      <Routes>
+      
+        <Route path='/' element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/Service" element={<Service/>} />
+        </Routes>
         
-        <Route path="/" component={Home} />
-        <Route path="/About" component={About} />
-        <Route path="/Service" component={Service} />
-        <Route path="/Contact" element={<Contact/>} />
-        {/* <Redirect to="/" /> */}
+      </BrowserRouter>
 
-      </Router>
+
     </>
-  
+
   );
 }
 
